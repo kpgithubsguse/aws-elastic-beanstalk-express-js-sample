66593e8e878cfbca058d22e67b39a83916a0c93b
Krishna Prakash <someone@nowhere.com>
2021-09-28 13:41:26 +0800

    Text change made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to first AWS project learning !'));
+app.get('/', (req, res) => res.send('Welcome to first AWS project learning  started!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
